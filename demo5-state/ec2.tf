652feaa3bd83e30642a374b9e7bd7c2bd9d0621f
AL-DevOps <96539143+AL-DevOps@users.noreply.github.com>
2022-10-27 20:25:28 +0300

    Create ec2.tf

---
+++ demo5-state/ec2.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "andrey-example" {
+  ami           = var.AMIS[var.region]
+  instance_type = "t2.micro"
+  subnet_id= "subnet-06ce4996838ec4d88"
+  vpc_security_group_ids = ["sg-04f2d5d1786a702f1"]
+  tags = {
+    Name = "andrey-demo5-state"
+  }
+  
+  provisioner "local-exec" {
+    command = "echo private ip: ${aws_instance.andrey-example.private_ip} >> ips.txt"
+  }
+  provisioner "local-exec" {
+    command = "echo public ip: ${aws_instance.andrey-example.public_ip} >> ips.txt"
+  }
+}
+
+output "ip" {
+  value = aws_instance.andrey-example.public_ip
+}
